744d4b24c9d4fa2abfe95e28659b00d566672e19
Mitsunori Kubota <mitsunori@asial.co.jp>
2014-03-08 02:31:58 +0900

    Added the topdoc-template files for themes pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-css');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-topdoc');
   var gruntConfig = require('./grunt-config.json');
   grunt.initConfig(gruntConfig);
   grunt.registerTask('css', ["compass:prod"]);
