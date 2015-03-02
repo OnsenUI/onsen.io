52a8cbdf5156c60fa57ac5a0ad4de5dab6c514bc
Andreas Argelius <andreas@argeli.us>
2015-03-02 20:41:56 +0900

    Updated CircleCI scripts.

---
+++ scripts/deploy_production.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+pushd $DIR/../OnsenUI/css-components
+gulp build
+popd
+
+pushd $DIR/../OnsenUI
+gulp build
+popd
+
+pushd $DIR/..
+echo "{\"key\": \"${AWS_KEY}\", \"secret\": \"${AWS_SECRET}\", \"bucket\": \"${AWS_BUCKET_EN}\", \"region\": \"${AWS_REGION_EN}\"}" > aws_en.json 
+gulp deploy --lang en --production
+echo "{\"key\": \"${AWS_KEY}\", \"secret\": \"${AWS_SECRET}\", \"bucket\": \"${AWS_BUCKET_JA}\", \"region\": \"${AWS_REGION_JA}\"}" > aws_ja.json 
+gulp deploy --lang ja --production
+popd
