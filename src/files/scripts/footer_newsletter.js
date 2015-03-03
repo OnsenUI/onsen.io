e759efd43dc6c5d2adf764a104eb049daec1ef76
Mitsunori Kubota <mitsunori@asial.co.jp>
2015-03-03 19:55:43 +0900

    Added newsletter form on blog posts page.

--- src/files/scripts/footer_newsletter.js
+++ src/files/scripts/footer_newsletter.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.newsletter-footer form').submit(function() {
+  $('.newsletter form').submit(function() {
     var email = $('input[name=email]', this).val();
 
     $.post('https://monaca.mobi/ja/api/email/e458bcbcc4', {email: email})
@@ -7,7 +7,7 @@ $(function() {
         var ret = JSON.parse(data);
 
         if (ret.status !== undefined && ret.status === 'success') {
-          $('.newsletter-footer form').hide();
+          $('.newsletter form').hide();
           $('.newsletter-thankyou').show();
         } else {
           alert('Something wrong with the request. Sorry.');
