476f9b934ddd8e4061d56f741e56591f36156241
Mitsunori Kubota <mitsunori@asial.co.jp>
2015-03-31 20:08:09 +0900

    Replaced timeSince() function with moment.js on github widget.

--- src/files/scripts/github_commits.js
+++ src/files/scripts/github_commits.js
@@ -2,32 +2,6 @@ $(function() {
   var $model = $('.github-widget-commit').hide(),
     base_url = 'https://api.github.com/repos/OnsenUI/OnsenUI';
 
-  var timeSince = function(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-      return interval + ' years';
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-      return interval + ' months';
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-      return interval + ' days';
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-      return interval + ' hours';
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-      return interval + ' minutes';
-    }
-    return Math.floor(seconds) + ' seconds';
-  }
-
   $.ajax({
     url: base_url,
     dataType: 'jsonp',
@@ -57,7 +31,7 @@ $(function() {
         $copy.find('.github-widget-commit-author').text(commit.commit.author.name);
 
         $copy.find('.github-widget-commit-info a')
-          .text(timeSince(new Date(commit.commit.author.date)) + ' ago')
+          .text(moment(new Date(commit.commit.author.date)).fromNow())
           .attr('href', commit.html_url);
 
         if(commit.author) {
