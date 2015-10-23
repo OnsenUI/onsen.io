d951009b2c269f8a74ec89ee27dc6f094f21bbd6
anatoo <anatoo.jp@gmail.com>
2015-10-23 16:56:23 +0900

    Changed to refer Onsen UI 2 css-components on /2/reference/css.html.

--- modules/css-transform.js
+++ modules/css-transform.js
@@ -1,32 +1,32 @@
 var postcss = require('postcss');
 var fs = require('fs');
 
-var cssPath = __dirname + '/../OnsenUI/build/css/onsen-css-components.css';
-
 var transform = postcss(function(css) {
   css.eachRule(function (rule) {
     if (rule.selector === 'body' || rule.selector === 'html') {
       rule.removeSelf();
     } else {
-      rule.selector = '.ons-css ' + rule.selector;
+      rule.selector = rule.selector.split(/,/).map(function(selector) {
+        return '.ons-css ' + selector;
+      }).join(', ');
     }
   });
 });
 
-
 module.exports = function(lang) {
   return function(files, matalsmith, done) {
-    fs.readFile(cssPath, function(error, css) {
-      if (error) {
-        throw error;
-      }
+    setImmediate(done);
 
-      var header = '/* NOTE: This css file is NOT original onsen-css-components.css */\n';
-      var file = {};
-      file.contents = new Buffer(header + transform.process(css.toString('utf8')).css);
-      files['reference/onsen-css-components.css'] = file;
+    var css1 = fs.readFileSync(__dirname + '/../OnsenUI/build/css/onsen-css-components.css', 'utf8');
+    var css2 = fs.readFileSync(__dirname + '/../2/OnsenUI/build/css/onsen-css-components.css', 'utf8');
 
-      done();
-    });
+    var header = '/* NOTE: This css file is NOT original onsen-css-components.css. */\n';
+    var file1 = {};
+    var file2 = {};
+    file1.contents = new Buffer(header + transform.process(css1.toString('utf8')).css);
+    file2.contents = new Buffer(header + transform.process(css2.toString('utf8')).css);
+
+    files['reference/onsen-css-components.css'] = file1;
+    files['2/reference/onsen-css-components.css'] = file2;
   };
 };
