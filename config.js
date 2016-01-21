104d10d36ed55ba330ad4d5cf1a8c626489a1f37
Fran Dios <frankdiox@gmail.com>
2016-01-21 14:13:58 +0900

    Changed OnsenUI url to https

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ module.exports = function(language, isStaging) {
     },
     onsen: {
       name: 'Onsen UI Team',
-      url: 'http://onsen.io',
+      url: 'https://onsen.io',
       gravator: 'e7c2e1cc42c38f0fe0685749d172e8ab'
     },
     moongift: {
@@ -89,7 +89,7 @@ module.exports = function(language, isStaging) {
     categories: categories,
 
     en: {
-      url: 'http://onsen.io',
+      url: 'https://onsen.io',
       title: 'The Answer to PhoneGap UI Development | Onsen UI',
       description: 'Develop awesome looking and high performance HTML5 mobile apps with Onsen framework and UI components. Provides AngularJS and jQuery bindings.',
       keywords: '',
