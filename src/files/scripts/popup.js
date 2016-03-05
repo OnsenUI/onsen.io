bc72adb7653ac8673043b93f52fff822193d1b92
andipavllo <andi.pavllo@gmail.com>
2016-03-05 19:52:16 +0900

    Popup improvements.

--- src/files/scripts/popup.js
+++ src/files/scripts/popup.js
@@ -1,24 +1,20 @@
-const ONE_DAY = 1000 * 60 * 60 * 24;
 var el, container, transitionEvent;
+var oneDay = 1000 * 60 * 60 * 24;
+var basicPopupDelay = 15000;
+var scrollPopupDelay = 1000;
 
-function testScroll(ev){
+function testScroll() {
   var body = document.body;
   var html = document.documentElement;
   var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
   if (window.scrollY > height/2) {
     window.onscroll = "";
-    if (typeof(Storage) !== "undefined") {
-      setTimeout(function() {
-        checkPopup();
-      }, 1000)
-    }
+    checkPopup(scrollPopupDelay);
   }
 }
-window.onscroll=testScroll;
 
 function whichTransitionEvent() {
-  var fakeEl = document.createElement('fakeElement');
   var transitions = {
     'transition':'transitionend',
     'OTransition':'oTransitionEnd',
@@ -27,13 +23,13 @@ function whichTransitionEvent() {
   }
 
   for (var t in transitions) {
-    if (fakeEl.style[t] !== undefined) {
+    if (el.style[t] !== undefined) {
         return transitions[t];
     }
   }
 }
 
-function onPopupClose(event) {
+function onPopupClose(e) {
   container.classList.add("hidden");
   document.body.style.position = "";
   document.body.style.overflow = "scroll";
@@ -44,21 +40,22 @@ function onPopupClose(event) {
 
 
 function setLocalStorage(popupName, now, days) {
-  var expireDate =  now + (ONE_DAY * days);
+  var expireDate =  now + (oneDay * days);
 
-  localStorage.setItem(popupName, expireDate);
+  localStorage.setItem(popupName + ".latestUpdate", expireDate);
   localStorage.setItem("latestUpdate", now);
 }
 
 function hidePopup(e) {
-  transitionEvent = whichTransitionEvent();
   var target = e.target;
-  var email = document.getElementsByClassName("input-email-popup")[0].value;
-  var delay = 1000;
+  var email = el.getElementsByTagName("input")[0].value;
+  var delay = 0;
+
+  transitionEvent = whichTransitionEvent();
 
   if (target === container || target.classList.contains("button-close") || (target.id === "button-submit" && email)) {
     if (e.target.id === "button-submit") {
-      delay = delay + 500;
+      delay = delay + 1000;
     }
 
     setTimeout(function() {
@@ -68,15 +65,18 @@ function hidePopup(e) {
   }
 }
 
-function canUpdateStorage(storage, now) {
-  return (!localStorage.latestUpdate || (storage - now <= 0) && ((localStorage.lastestUpdate + ONE_DAY) - now <= 0));
+function canUpdateStorage(popupName, now) {
+  var latestStorageUpdate = parseInt(localStorage.getItem("latestUpdate"));
+  var latestPopupUpdate = parseInt(localStorage.getItem(popupName + ".latestUpdate"));
+
+  return (!latestStorageUpdate || ((latestPopupUpdate - now <= 0) && (latestStorageUpdate + oneDay - now <= 0)));
 }
 
 function showPopup(popupName, now) {
   el = document.getElementById(popupName);
   container = document.getElementById("container-popup");
 
-  setLocalStorage(name, now, 30);
+  setLocalStorage(popupName, now, 30);
   el.classList.remove("hidden");
   container.classList.remove("hidden");
   document.body.style.overflow = "hidden";
@@ -84,10 +84,8 @@ function showPopup(popupName, now) {
   if (isMobile()) {
     el.classList.add("popup-mobile");
     document.body.style.position = "fixed";
-    if (popupName === "popup-newsletter") {
-      var emailInput = document.getElementById("input-email");
-      emailInput.style.width = "200px";
-    }
+    var emailInput = el.getElementsByTagName("input")[0];
+    emailInput.style.width = "200px";
   } else {
     el.classList.add("popup-desktop");
   }
@@ -97,26 +95,33 @@ function isMobile() {
   return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
 }
 
-function checkPopup() {
-  //localStorage.clear();
-  var popup = ["popup-newsletter", "popup2", "popup3"];
-  var now = new Date().getTime();
+function checkPopup(delay) {
+  if (typeof(Storage) !== "undefined") {
+    setTimeout(function() {
+      var popup = ["popup-newsletter", "popup-google-maps", "popup-material-design"];
+      var now = new Date().getTime();
 
-  for (var i in popup) {
-    popupName = popup[i];
-    var storage = "localStorage." + popupName;
-    if (canUpdateStorage(storage, now)) {
-      showPopup(popupName, now);
-      document.addEventListener("click", hidePopup, false);
-      return;
-    }
+      for (var i in popup) {
+        popupName = popup[i];
+        if (canUpdateStorage(popupName, now)) {
+          showPopup(popupName, now);
+          document.addEventListener("click", hidePopup, false);
+          return;
+        }
+      }
+    }, delay);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  if (typeof(Storage) !== "undefined") {
-    setTimeout(function() {
-      checkPopup();
-    }, 15000)
+function checkPopupEvent(e) {
+  if (e.type === "scroll") {
+    window.onscroll = testScroll;
+  } else {
+    checkPopup(basicPopupDelay);
   }
-});
+  document.removeEventListener(e.type, checkPopupEvent);
+}
+
+document.addEventListener("DOMContentLoaded", checkPopupEvent);
+
+document.addEventListener("scroll", checkPopupEvent);
