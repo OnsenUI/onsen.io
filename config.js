c859ad992060868056bc709bb22171fd23c2e5e9
Didi Yordanova <didi@asial.co.jp>
2016-04-19 12:09:27 +0900

    Update config and header, remove unneeded files

--- config.js
+++ config.js
@@ -3,66 +3,66 @@ var md5 = require('MD5');
 
 module.exports = function(language, isStaging) {
   var authors = {
-    andreas: {
-      name: 'Andreas Argelius',
-      email: 'andreas@asial.co.jp',
-      url: 'http://argeli.us'
-    },
-    anatoo: {
-      name: 'Mitsunori Kubota',
-      email: 'anatoo.jp@gmail.com',
-      url: 'http://anatoo.jp'
-    },
-    masa: {
-      name: 'Masa Tanaka',
-      email: 'masahiro@asial.co.jp',
-      url: 'http://www.asial.co.jp/en/',
-      gravator: '0b0ed6f4c5962b764d9bdb0ade4aa7ae'
-    },
-    fran: {
-      name: 'Fran Dios',
-      email: 'fran@asial.co.jp',
-      url: 'http://frandiox.com/en/',
-      image: '/blog/content/images/2015/Feb/yo2.jpg'
-    },
-    andi: {
-      name: 'Andi Pavllo',
-      email: 'andi@asial.co.jp',
-      image: '/blog/content/images/2015/Feb/1423814226357.jpg'
-    },
-    kruy: {
-      name: 'Kruy Vanna',
-      email: 'kruy@asial.co.jp'
-    },
-    onsen: {
-      name: 'Onsen UI Team',
-      url: 'https://onsen.io',
-      gravator: 'e7c2e1cc42c38f0fe0685749d172e8ab'
-    },
-    moongift: {
-      name: 'Atsushi Nakatsugawa',
-      gravator: '4cafe6a1c6287d64d7252279eeeffa94'
-    },
-    takuya: {
-      name: 'Takuya Watanabe',
-      gravator: '7ff040f6fd196a1dbffaea6572095969'
-    },
-    hosoi: {
-      name: 'Kazuho Hosoi',
-      gravatar: 'a963b1289748255706b7d630b5e6514f'
-    },
-    una: {
-      name: 'Una Softic',
-      email: 'una.softic@gmail.com'
-    },
-    patrick: {
-      name: 'Patrick Klitzke',
-      email: 'patrick@asial.co.jp'
-    },
-    bryan: {
-      name: 'Bryan Ellis',
-      gravator: '12e050bf226a74ef0c65fe0a637f6a31'
-    }
+      andreas: {
+        name: 'Andreas Argelius',
+        email: 'andreas@asial.co.jp',
+        url: 'http://argeli.us'
+      },
+      anatoo: {
+        name: 'Mitsunori Kubota',
+        email: 'anatoo.jp@gmail.com',
+        url: 'http://anatoo.jp'
+      },
+      masa: {
+        name: 'Masa Tanaka',
+        email: 'masahiro@asial.co.jp',
+        url: 'http://www.asial.co.jp/en/',
+        gravator: '0b0ed6f4c5962b764d9bdb0ade4aa7ae'
+      },
+      fran: {
+        name: 'Fran Dios',
+        email: 'fran@asial.co.jp',
+        url: 'http://frandiox.com/en/',
+        image: '/blog/content/images/2015/Feb/yo2.jpg'
+      },
+      andi: {
+        name: 'Andi Pavllo',
+        email: 'andi@asial.co.jp',
+        image: '/blog/content/images/2015/Feb/1423814226357.jpg'
+      },
+      kruy: {
+        name: 'Kruy Vanna',
+        email: 'kruy@asial.co.jp'
+      },
+      onsen: {
+        name: 'Onsen UI Team',
+        url: 'https://onsen.io',
+        gravator: 'e7c2e1cc42c38f0fe0685749d172e8ab'
+      },
+      moongift: {
+        name: 'Atsushi Nakatsugawa',
+        gravator: '4cafe6a1c6287d64d7252279eeeffa94'
+      },
+      takuya: {
+        name: 'Takuya Watanabe',
+        gravator: '7ff040f6fd196a1dbffaea6572095969'
+      },
+      hosoi: {
+        name: 'Kazuho Hosoi',
+        gravatar: 'a963b1289748255706b7d630b5e6514f'
+      },
+      una: {
+        name: 'Una Softic',
+        email: 'una.softic@gmail.com'
+      },
+      patrick: {
+        name: 'Patrick Klitzke',
+        email: 'patrick@asial.co.jp'
+      },
+      bryan: {
+        name: 'Bryan Ellis',
+        gravator: '12e050bf226a74ef0c65fe0a637f6a31'
+      }
   };
 
   for (author in authors) {
@@ -74,27 +74,50 @@ module.exports = function(language, isStaging) {
   }
 
   var categories = {
-    'showcase': {
-      name: 'Showcase',
-      title: 'Showcase of apps made with Onsen UI and Monaca'
+    en: {
+      showcase: {
+        name: 'Showcase',
+        title: 'Showcase of apps made with Onsen UI and Monaca'
+      },
+      announcement: {
+        name: 'Announcements',
+        title: 'Announcements of new features in Monaca and Onsen UI'
+      },
+      development: {
+        name: 'Development',
+        title: 'Articles about hybrid app development, HTML5, CSS and more'
+      },
+      tutorial: {
+        name: 'Tutorial',
+        title: 'Tutorials on how to create hybrid mobile apps, web apps, HTML5, CSS and JavaScript'
+      }
     },
-    'announcement': {
-      name: 'Announcements',
-      title: 'Announcements of new features in Monaca and Onsen UI'
-    },
-    'development': {
-      name: 'Development',
-      title: 'Articles about hybrid app development, HTML5, CSS and more'
-    },
-    'tutorial': {
-      name: 'Tutorial',
-      title: 'Tutorials on how to create hybrid mobile apps, web apps, HTML5, CSS and JavaScript'
+
+    ja: {
+      news: {
+        name: 'ニュース',
+        title: 'Monaca と　Onsen UI に関するニュース'
+      },
+      development: {
+        name: '技術情報',
+        title: 'ハイブリッドアプリ開発、HTML5、CSSなどに関する技術情報'
+      },
+      events: {
+        name: 'イベント情報',
+        title: 'Monaca と　Onsen UI に関するイベント情報'
+      },
+      tutorial: {
+        name: '事例紹介',
+        title: 'ハイブリッドアプリ開発、Webアプリ、 HTML5 、 CSS 、およびJavaScriptについてのチュートリアル'
+      }
     }
   };
 
+
+
   var env = {
     authors: authors,
-    categories: categories,
+    categories: categories[language],
 
     en: {
       url: 'https://onsen.io',
