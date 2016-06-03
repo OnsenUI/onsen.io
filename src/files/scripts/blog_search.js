461b9eee3fbe3d74e0216fbdbcef182127c4ae04
andipavllo <andi.pavllo@gmail.com>
2016-06-03 16:45:56 +0900

    Second draft.

--- src/files/scripts/blog_search.js
+++ src/files/scripts/blog_search.js
@@ -1,20 +1,36 @@
 (function() {
+  var searchBar;
+  var searchIcon;
+  var searchContainer;
+
   document.addEventListener("DOMContentLoaded", searchInit);
 
   function searchInit() {
-    var searchIcon = document.getElementsByClassName("blog-header-search")[0];
-
+    searchIcon = document.getElementsByClassName("blog-header-search")[0];
     searchIcon.addEventListener("click", enableSearchBar);
   }
 
   function enableSearchBar() {
+    searchContainer = document.getElementsByClassName("blog-header-search-container-hidden")[0];
+    searchContainer.className = "blog-header-search-container blog-header-search-container-visible";
 
-    var searchBar = document.getElementsByClassName("blog-header-search-bar-hidden")[0];
-    searchBar.className = "blog-header-search-bar-visible";
+    searchBar = document.getElementsByClassName("blog-header-search-bar-hidden")[0];
 
-    searchBar.addEventListener("click", enlargeBar);
+    setTimeout(function() {
+      searchBar.classList.remove("blog-header-search-bar-hidden");
+      searchBar.classList.add("blog-header-search-bar-visible");
+
+      document.addEventListener("click", searchClickOnSearch);
+      searchIcon.removeEventListener("click", enableSearchBar);
+    }, 100);
   }
 
-  function enlargeBar(e) {
+  function searchClickOnSearch(e) {
+    if(e.target != searchBar) {
+      searchBar.className = "blog-header-search-bar blog-header-search-bar-hidden";
+      searchBar.parentElement.className =  "blog-header-search-container blog-header-search-container-hidden";
+      document.removeEventListener("click", searchClickOnSearch);
+      searchIcon.addEventListener("click", enableSearchBar);
+    }
   }
 })();
