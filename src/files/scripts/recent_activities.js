db8a31ef3eafc48d0618804eda150d29fe142f64
Naoki Matagawa <naoki.m@asial.co.jp>
2017-09-06 23:28:09 +0900

    Change Recent Activities to use remote data on GitHub.

--- src/files/scripts/recent_activities.js
+++ src/files/scripts/recent_activities.js
@@ -2,120 +2,128 @@ $(function() {
   // Key for saving "last read date" into the Local Storage
   var KEY = 'recent-activities.lastReadDate';
 
+  var requestUrl;
+  switch (location.hostname) {
+    case 'onsen.io':
+    case 's.onsen.io':
+      // GitHub repo: OnsenUI/recent-activities
+      // Branch: master
+      // Path: /recent-activities-en.md
+      requestUrl = 'https://raw.githubusercontent.com/OnsenUI/recent-activities/master/recent-activities-en.json';
+      break;
+    case 'ja.onsen.io':
+    case 's.ja.onsen.io':
+      // GitHub repo: OnsenUI/recent-activities
+      // Branch: master
+      // Path: /recent-activities-ja.md
+      requestUrl = 'https://raw.githubusercontent.com/OnsenUI/recent-activities/master/recent-activities-ja.json';
+    break;
+    case 'localhost':
+    case '127.0.0.1':
+      // GitHub repo: OnsenUI/recent-activities
+      // Branch: dev
+      // Path: /recent-activities-en.md
+      requestUrl = 'https://raw.githubusercontent.com/OnsenUI/recent-activities/dev/recent-activities-en.json';
+      break;
+    default:
+      requestUrl = null;
+  }
+
   // Load recent activities data from GitHub via XHR
-
-  // Replace "loading..." place holder with actual content
-  // based on the recent activities data
-  new Vue({
-    el: '#recent-activities-placeholder',
-    template: '#recent-activities-template',
-    data: {
-      lastReadDate: moment('2000-01-01T00:00:00+00:00'),
-      isLocalStorageSupported: (function() {
-        try {
-          localStorage.setItem('test_local_storage', 'foo');
-          localStorage.removeItem('test_local_storage');
-          return true;
-        } catch(e) {
-          return false;
-        }
-      })(),
-      items: [
-        {
-          date: '2017-09-01T00:00:00+09:00',
-          title: `<a href="/react/" style="color: #444;">react-onsenui</a> 1.7.0 Released`,
-          description: `
-            <p>
-                Supported React Fiber (React 16).
-                <a href="asdf">Release Note</a>
-                <br>
-                <code>npm install react-onsenui --save</code>
-            </p>
-          `
-        },
-        {
-          date: '2017-08-14T00:00:00+09:00',
-          title: `Onsen UI 2.6.0 Released`,
-          description: `
-            <p>
-              New features are available.
-              <a href="asdf">Release Note</a>
-            </p>
-            <ul>
-              <li>Swiping tabbar</li>
-              <li>Segment</li>
-              <li>New color scheme for iOS</li>
-            </ul>
-            <p>
-              <code>npm install onsenui --save</code>
-            </p>
-          `
-        },
-        {
-          date: '2017-08-02T00:00:00+09:00',
-          title: `Website Update`,
-          description: `
-            <p>
-              Added <a href="asdf">Component Compilation</a> section.
-            </p>
-          `
-        }
-      ]
-    },
-    created: function() {
-      // Read the last read date from the Local Storage
-      if (this.isLocalStorageSupported) {
-        var savedValue = localStorage.getItem(KEY);
-
-        if (savedValue) {
-          // Parse as a UNIX Timestamp (seconds)
-          this.lastReadDate = moment.unix(savedValue);
-        }
+  if (requestUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', requestUrl);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var recentActivitiesData = JSON.parse(xhr.responseText);
+        onSuccess(recentActivitiesData);
       }
-    },
-    methods: {
-      updateLastReadDate: function() {
-        // Update data
-        this.lastReadDate = moment();
+      else {
+        onFailure();
+      }
+    };
+    xhr.send();
+  }
 
-        // Update local storage
+  function onSuccess(recentActivitiesData) {
+    // console.log(recentActivitiesData);
+
+    // Replace "loading..." place holder with actual content
+    // based on the recent activities data
+    new Vue({
+      el: '#recent-activities-placeholder',
+      template: '#recent-activities-template',
+      data: {
+        lastReadDate: moment('2000-01-01T00:00:00+00:00'),
+        isLocalStorageSupported: (function() {
+          try {
+            localStorage.setItem('test_local_storage', 'foo');
+            localStorage.removeItem('test_local_storage');
+            return true;
+          } catch(e) {
+            return false;
+          }
+        })(),
+        items: recentActivitiesData
+      },
+      created: function() {
+        // Read the last read date from the Local Storage
         if (this.isLocalStorageSupported) {
-          // Save as a UNIX Timestamp (seconds)
-          localStorage.setItem(KEY, this.lastReadDate.unix());
-        }
-      },
-      fromNow: function(date) {
-        return moment.parseZone(date).fromNow();
-      },
-      format: function(date) {
-        return moment.parseZone(date).format('lll');
-      },
-      countUnreadItems: function() {
-        // If Local Storage is not supported, mark all items as read
-        if (!this.isLocalStorageSupported) {
-          return 0;
-        }
+          var savedValue = localStorage.getItem(KEY);
 
-        return this.items
-        .filter(this.isNew)
-        .filter(this.isUnread).length;
+          if (savedValue) {
+            // Parse as a UNIX Timestamp (seconds)
+            this.lastReadDate = moment.unix(savedValue);
+          }
+        }
       },
-      isNew: function(item) {
+      methods: {
+        updateLastReadDate: function() {
+          // Update data
+          this.lastReadDate = moment();
+
+          // Update local storage
+          if (this.isLocalStorageSupported) {
+            // Save as a UNIX Timestamp (seconds)
+            localStorage.setItem(KEY, this.lastReadDate.unix());
+          }
+        },
+        fromNow: function(date) {
+          return moment.parseZone(date).fromNow();
+        },
+        format: function(date) {
+          return moment.parseZone(date).format('lll');
+        },
+        countUnreadItems: function() {
+          // If Local Storage is not supported, mark all items as read
+          if (!this.isLocalStorageSupported) {
+            return 0;
+          }
+
+          return this.items
+          .filter(this.isNew)
+          .filter(this.isUnread).length;
+        },
+        isNew: function(item) {
           // Ignore future events
           if (moment.parseZone(item.date) > moment()) {
             return false;
           }
-        // If the date of item is within the past 7 days
-        if (moment.parseZone(item.date) > moment().subtract(7, 'day')) {
-          return true;
-        }
-      },
-      isUnread: function(item) {
-        // If the date of item is after the last read date
-        if (moment.parseZone(item.date) > this.lastReadDate) {
-          return true;
+          // If the date of item is within the past 7 days
+          if (moment.parseZone(item.date) > moment().subtract(7, 'day')) {
+            return true;
+          }
+        },
+        isUnread: function(item) {
+          // If the date of item is after the last read date
+          if (moment.parseZone(item.date) > this.lastReadDate) {
+            return true;
+          }
         }
       }
-    }
-  });  
+    });
+  }
+
+  function onFailure() {
+  }
 });
