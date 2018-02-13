c937fc4ea95bfb566cca3abc8d0d354cee50f745
Eric Corson <emccorson@gmail.com>
2018-02-13 15:46:16 +0900

    Add subsections and auto-scrolling to current page in API side menu
    API side menu now uses same code in default.js as Guide side menu with
    parameters passed in from side-menu-highlight-apis.js and
    side-menu-highlight.js respectively

--- src/files/scripts/side-menu-highlight-api.js
+++ src/files/scripts/side-menu-highlight-api.js
@@ -1,7 +1,28 @@
-// side menu highlight for API pages
-(function() {
-  var mainId = window.location.pathname;
-  var link = $('a[href="' + mainId + '"]');
-  link.addClass('current');
-  link.parent('li').addClass('toc-item-open');
+(function () {
+  function getParams() {
+    var mainId = window.location.pathname;
+    var mainLink = $('.content-info a[href="' + mainId + '"]');
+
+    var links = mainLink.parent('li').find('.toc-2-link');
+    var linkMap = {};
+    var sections = [];
+    // main link
+    linkMap['#main'] = mainLink;
+    sections.push($('#main'));
+    // sub links
+    for (var i = 0; i < links.length; i++) {
+      var link = $(links[i]);
+      var sectionId = '#' + link.attr('href').split('#').pop();
+      var section = $(sectionId);
+      sections.push(section);
+      linkMap[sectionId] = link;
+    }
+
+    return { mainLink: mainLink
+           , linkMap: linkMap
+           , sections: sections
+           };
+  };
+
+  sideMenuHighlight(getParams);
 })();
--- src/files/scripts/side-menu-highlight.js
+++ src/files/scripts/side-menu-highlight.js
@@ -1,81 +1,25 @@
-// side menu highlight
-(function() {
-  var links = $('.content-info a');
-  var sections = [];
-  var linkMap = {};
-  var toc1Items = $('.toc-1-item');
+(function () {
+  function getParams() {
+    var mainId = $('.container-content h3:first-of-type')[0].id;
+    var mainLink =  $('.content-info a[href$="#' + mainId + '"]');
 
-  var scrollWrapper = document;
-  var offset = 200;
+    var links = mainLink.parent('li').find('a');
+    var linkMap = {};
+    var sections = [];
 
-  if (links.length > 0) {
-    prepare();
-    setTimeout(update, 400);
-    scrollWrapper.addEventListener('scroll', queueUpdate, true);
-
-    // Select current item in ToC
-    var mainID = $('.container-content h3:first-of-type')[0].id
-    var el = linkMap['#' + mainID].link;
-    linkMap['#' + mainID].link.addClass('current');
-    linkMap['#' + mainID].link.parent('li').addClass('toc-item-open');
-    // Show current item in screen
-    var elOffset = el.offset();
-    var menu = $('.content-info');
-    if (elOffset && elOffset.top > menu.height() / 2) {
-      setTimeout(function() {
-        menu.scrollTop(elOffset.top - menu.offset().top - menu.height()/2);
-      }, 0)
-    }
-  }
-
-  var queued = false;
-  function queueUpdate() {
-    if (!queued) {
-      queued = true;
-      setTimeout(function() {
-        update();
-        queued = false;
-      }, 80);
-    }
-  }
-
-  function update() {
-    var scrolled = window.scrollY + 50;
-
-    for (var i = sections.length - 1; i >= 0; i--) {
-      var section = sections[i];
-      if (typeof section.offset() == 'undefined') {
-
-      } else {
-        var position = section.offset().top;
-      }
-
-      if (scrolled > position) {
-        var id = '#' + section.attr('id');
-        links.removeClass('current');
-        toc1Items.removeClass('toc-item-open');
-        linkMap[id].link.addClass('current');
-        $(linkMap[id].link.parents('.toc-1-item')[0]).addClass('toc-item-open');
-        return;
-      }
-    };
-  }
-
-  function prepare() {
     for (var i = 0; i < links.length; i++) {
       var link = $(links[i]);
-      var id = '#' + link.attr('href').split('#').pop();
-      try {
-        var section = $(id);
-        if (section) {
-          sections.push(section);
-          linkMap[id] = {};
-          linkMap[id].link = link;
-        }
-      } catch (e) {
-        // ignore
-      }
+      var sectionId = '#' + link.attr('href').split('#').pop();
+      var section = $(sectionId);
+      sections.push(section);
+      linkMap[sectionId] = link;
     }
-  }
 
+    return { mainLink: mainLink
+           , linkMap: linkMap
+           , sections: sections
+           };
+  };
+
+  sideMenuHighlight(getParams);
 })();
